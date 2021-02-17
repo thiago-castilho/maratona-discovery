07cb558c355d0ff27a1f46dc3f4718b2af346484
Thiago Castilho <tcosta1996@hotmail.com>
2021-02-17 03:49:10 -0300

    Correção do bug dos valores quebrados começando com 0

--- scripts.js
+++ scripts.js
@@ -120,8 +120,8 @@ const Utils = {
     },
 
     formatAmount(value) {
-        value = Number(value) * 100;
-        return value
+        value = value * 100;
+        return Math.round(value);
     },
 
     formatCurrency(value) {
